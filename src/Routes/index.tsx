1f3faa93648e61b3581f2396d41b479508151d93
Adedipe Oluwatobi <toby_adedipe@yahoo.com>
2023-07-06 02:03:39 +0100

    first commit

---
+++ src/Routes/index.tsx
@@ -0,0 +1,16 @@
+import {Route, Routes} from "react-router-dom";
+import MainPage from "../Pages/MainPage";
+import Product from "../Pages/Product";
+import ProductEdit from "../Pages/Product/Edit";
+
+const CustomRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/product/edit" element={<ProductEdit />} />
+    </Routes>
+  )
+}
+
+export default CustomRoutes;
